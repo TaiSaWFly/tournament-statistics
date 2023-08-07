1e608b2c11ce1c7198311d311a4d2cf827afca81
TaiSaWFly <vawer24@gmail.com>
2023-08-07 04:41:17 +0300

    clear log

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
 import App from "./app/App";
 
-console.log(process.env);
-
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
