a7c8c9d623291dc9a9059f2fe6799d8bd01f9fd4
TaiSaWFly <vawer24@gmail.com>
2023-09-01 00:08:12 +0300

    edit-display-team

--- src/app/components/ui/PlayerComponents/PlayerTournamentComponents/PlayerTournamentItemComponents/PlayerTournamentItemActions/PlayerTournamentItemActions.tsx
+++ src/app/components/ui/PlayerComponents/PlayerTournamentComponents/PlayerTournamentItemComponents/PlayerTournamentItemActions/PlayerTournamentItemActions.tsx
@@ -4,57 +4,30 @@ import Button from "../../../../../common/Button/Button";
 import useSvgIcon from "../../../../../../hooks/appHooks/useSvgIcon";
 
 interface PlayerTournamentItemActionsProps {
-    isOpenTeamResult: boolean;
     isOpenTeamMatches: boolean;
-    onOpenTeamResult: () => void;
     onOpenTeamMatches: () => void;
 }
 
 const PlayerTournamentItemActions: React.FC<
     PlayerTournamentItemActionsProps
-> = ({
-    isOpenTeamResult,
-    isOpenTeamMatches,
-
-    onOpenTeamResult,
-    onOpenTeamMatches
-}) => {
-    const { Note, Book } = useSvgIcon();
+> = ({ isOpenTeamMatches, onOpenTeamMatches }) => {
+    const { Book } = useSvgIcon();
 
     return (
-        <>
-            <div className={style.action__action_btn}>
-                <Button
-                    onClick={onOpenTeamResult}
-                    className={
-                        isOpenTeamResult ? style.action__action_btn_active : ""
-                    }
-                >
-                    <div className={style.action__action_icon}>
-                        <Note />
-                    </div>
-                </Button>
+        <div className={style.action__action_btn}>
+            <Button
+                onClick={onOpenTeamMatches}
+                className={
+                    isOpenTeamMatches ? style.action__action_btn_active : ""
+                }
+            >
+                <div className={style.action__action_icon}>
+                    <Book />
+                </div>
+            </Button>
 
-                <span className={style.action__action_btn__info}>
-                    результаты
-                </span>
-            </div>
-
-            <div className={style.action__action_btn}>
-                <Button
-                    onClick={onOpenTeamMatches}
-                    className={
-                        isOpenTeamMatches ? style.action__action_btn_active : ""
-                    }
-                >
-                    <div className={style.action__action_icon}>
-                        <Book />
-                    </div>
-                </Button>
-
-                <span className={style.action__action_btn__info}>матчи</span>
-            </div>
-        </>
+            <span className={style.action__action_btn__info}>матчи</span>
+        </div>
     );
 };
 
