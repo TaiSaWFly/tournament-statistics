29d2bcb3dcd86ab9d8a399403452d47188fcfd07
Stanislav Kadykov <102415183+TaiSaWFly@users.noreply.github.com>
2024-05-21 03:14:27 +0300

    Update links LOGS

--- src/app/data/OtherData/links.ts
+++ src/app/data/OtherData/links.ts
@@ -1,7 +1,7 @@
 const GOOGLE_STATISTICS_TOURNAMENTS_LINK =
     "https://docs.google.com/spreadsheets/d/1eqIpTxPxAeen23CN9XDChg_-mTmXCJtpdkDF8B1Os-8/edit#gid=255048783";
 const LOGS_STATISTICS_TOURNAMENTS_LINK =
-    "https://app.powerbi.com/view?r=eyJrIjoiZTJkZjE0YmYtNTNlMi00OTI0LWI1OTktYjkzMmI5NGI5N2ZjIiwidCI6ImM1ZGU0Mjk3LWE2MDAtNGMwNy04Y2Y0LTFhYTVmMDA1NzZjZCIsImMiOjl9";
+    "https://app.powerbi.com/view?r=eyJrIjoiZjI5NmU4Y2QtODkxZS00MWE3LWFiN2MtZjNlMDM3ZjVlMzg1IiwidCI6ImQyNWIzY2U2LWVmNDctNDMwMS04MGZhLTgwMzIzZmJkMDYzZSJ9";
 const REPO_STATISTICS_TOURNAMENTS_LINK =
     "https://github.com/TaiSaWFly/tournament-statistics";
 
